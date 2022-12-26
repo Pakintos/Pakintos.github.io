cc344088c8067659d2e1d1272263c60cb8ba8e37
Pakintos <121130979+Pakintos@users.noreply.github.com>
2022-12-26 17:04:19 +0100

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,57 +1,18 @@
 <?php
-$GET_IMPUT = file_get_contents('php://imput');
 
-const TOKEN = '5694415530:AAHRFyPsjPkrwW2nx9xwA6yA8HO2E4_gt2c';
+include('vender/autoload.php');
+use Telegram\Bot\Api
 
-const API_URL = 'https://api.telegram.org/bot';
+$telegram = new Api('5694415530:AAHRFyPsjPkrwW2nx9xwA6yA8HO2E4_gt2c')';
+$result = $telegram->getWebhookUpdates();
 
-function printAnswer($str) {
-    echo "<pre>";
-    print_r($str);
-    echo "</pre>";
+$text = $result["message"]["text"];
+$chat_id = $result["message"]["chat"]["id"];
+$name = $result["message"]["from"]["username"];
+$first_name =$result["message"]["from"]["first_name"];
+$last_name = $result["message"]["from"]["last_name"];
+
+if($text == "/start"){
+    $reply = "Fack Off";
+    $telegram->sendMessage(['chat_id' => $chat_id,'text' => $reply]);
 }
-
-function getTelegramApi($method,$options = null) {
-
-    $str_request = API_URL . TOKEN . '/' . $method;
-
-    if ($options) {
-        $str_request .= '?' . http_build_query($options);
-    }
-$request = file_get_contents($str_request);
-return json_decode($request, 1);
-}
-
- function setHook($set = 1) {
- $url = 'htpps://' . $_SERVER ['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
- printAnswer(
-    getTelegramApi('setWebhook' ,
-         [
-            'url' => $set?$url:''    
-         ]
-    )
- );
-exit();
- }
-
- 
- setHook(1); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
